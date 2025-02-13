b2bdbe721c5424d762c21a9a93a048bda0dbbdf7
Kun-Yi Lin <108348635+kelvinlinkk@users.noreply.github.com>
2025-02-13 20:56:39 +0800

    initial project setup with HTML structure, CSS styles, and JavaScript functionality for dialog, audio, and button management

---
+++ script/main.js
@@ -0,0 +1,21 @@
+import DialogSystem from '../lib/dialog.js';
+import AudioManager from '../lib/audio.js';
+import ButtonManager from '../lib/button.js';
+const gameArea = document.querySelector("main");
+async function main(){
+    const dialog = new DialogSystem(gameArea);
+    const audio = new AudioManager(gameArea);
+    const btn = new ButtonManager(gameArea);
+    btn.addButton("123","kk");
+    btn.addButton("1234","k2k");
+    audio.addAudio("song","resources/Music.mp3");
+    await new Promise((r)=>{
+        gameArea.addEventListener("click",()=>{
+            audio.audPlay("song",0,0);r()})
+    })
+    dialog.show();
+    await dialog.readWords("gameArea append Child(dia  log.elem ts.box);");
+    window.alert(await btn.showButton());
+    dialog.hide();
+}
+main();
