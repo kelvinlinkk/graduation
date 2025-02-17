d631986a37980bb9ec0d4df40d21da30750c6cd0
Kun-Yi Lin <108348635+kelvinlinkk@users.noreply.github.com>
2025-02-17 20:24:53 +0800

    Add ImageManager class for image handling and animations

--- script/main.js
+++ script/main.js
@@ -1,13 +1,17 @@
 import DialogSystem from '../lib/dialog.js';
 import AudioManager from '../lib/audio.js';
 import ButtonManager from '../lib/button.js';
+import ImageManager from '../lib/image.js';
 const gameArea = document.querySelector("main");
 async function main(){
     const dialog = new DialogSystem(gameArea);
     const audio = new AudioManager(gameArea);
     const btn = new ButtonManager(gameArea);
-
-    dialog.setSpeaker("John");
+    const img = new ImageManager(gameArea);
+    img.addImg("Bocchi","resources/bocchi.jpg");
+    img.setAppearance("Bocchi",{ width:100});
+    await img.skew("Bocchi", 50,0,3);
+    await img.skew("Bocchi", -50,0,1);
     btn.addButton("123","kk");
     btn.addButton("1234","k2k");
     audio.addAudio("song","resources/Music.mp3");
@@ -17,7 +21,7 @@ async function main(){
     })
     dialog.show();
     await dialog.readWords("gameArea append Child(dia  log.elem ts.box);");
-    await dialog.readWords(await btn.showButton());
-    /*dialog.hide();*/
+    window.alert(await btn.showButton());
+    dialog.hide();
 }
 main();
