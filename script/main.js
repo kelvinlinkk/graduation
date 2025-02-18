40f1a3d681fd33c957d12c5c5c2268a2e61a1163
Kun-Yi Lin <108348635+kelvinlinkk@users.noreply.github.com>
2025-02-18 20:01:19 +0800

    Add audio and image resource management with JSON files; implement image visibility control in ImageManager

--- script/main.js
+++ script/main.js
@@ -2,23 +2,40 @@ import DialogSystem from '../lib/dialog.js';
 import AudioManager from '../lib/audio.js';
 import ButtonManager from '../lib/button.js';
 import ImageManager from '../lib/image.js';
+import loadSource from '../lib/util.js';
+
 const gameArea = document.querySelector("main");
-async function main(){
-    const dialog = new DialogSystem(gameArea);
-    const audio = new AudioManager(gameArea);
-    const btn = new ButtonManager(gameArea);
-    const img = new ImageManager(gameArea);
-    img.addImg("Bocchi","resources/bocchi.jpg");
+const dialog = new DialogSystem(gameArea);
+const audio = new AudioManager(gameArea);
+const btn = new ButtonManager(gameArea);
+const img = new ImageManager(gameArea);
+
+function setBackground(src){
+    let background = gameArea.appendChild(document.createElement("img"));
+    background.id = "bg";
+    background.src = src;
+    return background;
+}
+
+async function setup(){
+    dialog.setSpeaker("John");
+    dialog.setAppearance("resources/sunset.jpg","yellow");
+    await loadSource(img, audio);
+    let background = setBackground("resources/sunset.jpg");
     img.setAppearance("Bocchi",{ width:100});
-    await img.skew("Bocchi", 50,0,3);
-    await img.skew("Bocchi", -50,0,1);
+    return background;
+}
+
+async function main(){
+    const background = await setup();
     btn.addButton("123","kk");
     btn.addButton("1234","k2k");
-    audio.addAudio("song","resources/Music.mp3");
     await new Promise((r)=>{
         gameArea.addEventListener("click",()=>{
             audio.audPlay("song",0,0);r()})
     })
+    img.showImg("Bocchi");
+    await img.skew("Bocchi", -50,0,1);
     dialog.show();
     await dialog.readWords("gameArea append Child(dia  log.elem ts.box);");
     window.alert(await btn.showButton());
