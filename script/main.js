61d99fc37f42623d5fbdd5adee09775b9eb1ec39
kelvinlinkk <kelvinlinkk@outlook.com>
2025-02-19 14:56:40 +0800

    readstory(unfinished)

--- script/main.js
+++ script/main.js
@@ -2,7 +2,7 @@ import DialogSystem from '../lib/dialog.js';
 import AudioManager from '../lib/audio.js';
 import ButtonManager from '../lib/button.js';
 import ImageManager from '../lib/image.js';
-import loadSource from '../lib/util.js';
+import {loadSource,ReadStory} from '../lib/util.js';
 
 const gameArea = document.querySelector("main");
 const dialog = new DialogSystem(gameArea);
@@ -10,43 +10,47 @@ const audio = new AudioManager(gameArea);
 const btn = new ButtonManager(gameArea);
 const img = new ImageManager(gameArea);
 
-function setBackground(src){
+function setBackground(src) {
     let background = gameArea.appendChild(document.createElement("img"));
     background.id = "bg";
     background.src = src;
+    ReadStory("","story/preface.txt");
     return background;
 }
 
-async function setup(){
+async function setup() {
     dialog.setSpeaker("John");
-    dialog.setAppearance("none","yellow");
+    dialog.setAppearance("none", "yellow");
     await loadSource(img, audio);
     let background = setBackground("resources/sunset.jpg");
-    img.setAppearance("Bocchi",{ width:100});
+    img.setAppearance("Bocchi", { width: 100 });
     return background;
 }
 
-async function main(){
+async function main() {
     const background = await setup();
-    btn.addButton("So far, so good..","Fine");
-    btn.addButton("Not quite so well.","Bad");
-    await new Promise((r)=>{
-        gameArea.addEventListener("click",()=>{
-            audio.audPlay("song",0,0);r()})
+    btn.addButton("So far, so good..", "Fine");
+    btn.addButton("Not quite so well.", "Bad");
+    await new Promise((r) => {
+        gameArea.addEventListener("click", () => {
+            audio.audPlay("song", 0, 0); r()
+        })
     })
     img.showImg("Bocchi");
-    await img.skew("Bocchi", -50,0,1);
+    await img.skew("Bocchi", -50, 0, 1);
     dialog.show();
     dialog.readWords("Hello.");
-    await new Promise((r)=>{
-        gameArea.addEventListener("click",()=>{
-            r()})
+    await new Promise((r) => {
+        gameArea.addEventListener("click", () => {
+            r()
+        })
     })
     await dialog.readWords("How are you?");
     dialog.readWords(await btn.showButton());
-    await new Promise((r)=>{
-        gameArea.addEventListener("dblclick",()=>{
-            r()})
+    await new Promise((r) => {
+        gameArea.addEventListener("dblclick", () => {
+            r()
+        })
     })
     dialog.hide();
 }
