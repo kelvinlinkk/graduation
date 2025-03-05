c019f5f6bc7c2c73d7a8f46192fdfccac99cb687
Kun-Yi Lin <108348635+kelvinlinkk@users.noreply.github.com>
2025-03-05 20:10:57 +0800

    Enhance game functionality: add pause/resume feature, improve image loading with async, and update HTML structure for landing page

--- script/main.js
+++ script/main.js
@@ -1,4 +1,15 @@
 import { Game } from '../lib/game.js';
 
+const landing = document.getElementById('landing');
+const startbtn = document.getElementById('startbtn');
 const game = new Game();
-game.start();
+document.addEventListener('keydown',e =>{
+    if(e.key === 'Escape'){
+        game.togglePause();
+    }
+})
+startbtn.addEventListener('click',async e =>{
+    landing.style.display = 'none';
+    await game.start();
+    landing.style.display = "initial";
+})
