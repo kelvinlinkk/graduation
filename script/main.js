49495a690f71e00591d9bde65847277fa167c949
Kun-Yi Lin <108348635+kelvinlinkk@users.noreply.github.com>
2025-03-08 14:52:06 +0800

    Add settings menu and enhance dialog logging functionality, add save and load progress

--- script/main.js
+++ script/main.js
@@ -1,15 +1,36 @@
 import { Game } from '../lib/game.js';
 
+const setting = document.getElementById('setting');
+const settingbtn = document.getElementById('settingbtn');
 const landing = document.getElementById('landing');
 const startbtn = document.getElementById('startbtn');
 const game = new Game();
-document.addEventListener('keydown',e =>{
-    if(e.key === 'Escape'){
+
+const ICONS = {
+    SETTINGS: './assets/icon/setting.png',
+    LOGOUT: './assets/icon/logout.png'
+};
+
+document.addEventListener('keydown', e => {
+    if (e.key === 'Escape') {
         game.togglePause();
     }
 })
-startbtn.addEventListener('click',async e =>{
+
+settingbtn.addEventListener('click', () => {
+    const isSettingVisible = setting.style.display === 'flex';
+    setting.style.display = isSettingVisible ? 'none' : 'flex';
+    settingbtn.src = isSettingVisible ? ICONS.SETTINGS : ICONS.LOGOUT;
+});
+
+startbtn.addEventListener('click', async e => {
     landing.style.display = 'none';
-    await game.start();
+    const data = (localStorage.getItem('data'));
+    console.log(data)
+    if (data) {
+        await game.start(JSON.parse(data));
+    } else {
+        await game.start();
+    }
     landing.style.display = "initial";
 })
